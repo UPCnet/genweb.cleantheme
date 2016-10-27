5cb962c6a25df4215206ea1812db310342f3e5f1
Alberto Duran <alberto.duran@lpc51248.estacions.upcnet.es>
2016-10-27 12:09:35 +0200

    treure dynamic css

--- genweb/cleantheme/browser/interfaces.py
+++
@@ -1,5 +0,0 @@
-from genweb.upc.browser.interfaces import IGenwebUPC
-
-
-class IGenwebCleantheme(IGenwebUPC):
-    """Marker interface that defines a Zope browser layer."""
--- genweb/cleantheme/interfaces.py
+++ genweb/cleantheme/interfaces.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """Module where all interfaces, events and exceptions live."""
 
-from genweb.upc.browser.interfaces import IGenwebUPC
+from plone.theme.interfaces import IDefaultPloneLayer
 
 
-class IGenwebCleanthemeLayer(IGenwebUPC):
+class IGenwebCleanthemeLayer(IDefaultPloneLayer):
     """Marker interface that defines a Zope 3 browser layer."""
